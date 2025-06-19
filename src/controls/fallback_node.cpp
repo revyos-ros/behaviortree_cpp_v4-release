4e94313616738b99cd43e74739405d68a25d715d
Zheng Junjie <z572@z572.online>
2025-06-19 05:03:30 +0000

    Rebase from 'upstream'

--- src/controls/fallback_node.cpp
+++ src/controls/fallback_node.cpp
@@ -16,10 +16,7 @@
 namespace BT
 {
 FallbackNode::FallbackNode(const std::string& name, bool make_asynch)
-  : ControlNode::ControlNode(name, {})
-  , current_child_idx_(0)
-  , all_skipped_(true)
-  , asynch_(make_asynch)
+  : ControlNode::ControlNode(name, {}), current_child_idx_(0), asynch_(make_asynch)
 {
   if(asynch_)
     setRegistrationID("AsyncFallback");
@@ -31,9 +28,9 @@ NodeStatus FallbackNode::tick()
 {
   const size_t children_count = children_nodes_.size();
 
-  if(status() == NodeStatus::IDLE)
+  if(!isStatusActive(status()))
   {
-    all_skipped_ = true;
+    skipped_count_ = 0;
   }
 
   setStatus(NodeStatus::RUNNING);
@@ -44,9 +41,6 @@ NodeStatus FallbackNode::tick()
 
     auto prev_status = current_child_node->status();
     const NodeStatus child_status = current_child_node->executeTick();
-
-    // switch to RUNNING state as soon as you find an active child
-    all_skipped_ &= (child_status == NodeStatus::SKIPPED);
 
     switch(child_status)
     {
@@ -61,7 +55,7 @@ NodeStatus FallbackNode::tick()
       case NodeStatus::FAILURE: {
         current_child_idx_++;
         // Return the execution flow if the child is async,
-        // to make this interruptable.
+        // to make this interruptible.
         if(asynch_ && requiresWakeUp() && prev_status == NodeStatus::IDLE &&
            current_child_idx_ < children_count)
         {
@@ -73,6 +67,7 @@ NodeStatus FallbackNode::tick()
       case NodeStatus::SKIPPED: {
         // It was requested to skip this node
         current_child_idx_++;
+        skipped_count_++;
       }
       break;
       case NodeStatus::IDLE: {
@@ -82,19 +77,22 @@ NodeStatus FallbackNode::tick()
   }    // end while loop
 
   // The entire while loop completed. This means that all the children returned FAILURE.
+  const bool all_children_skipped = (skipped_count_ == children_count);
   if(current_child_idx_ == children_count)
   {
     resetChildren();
     current_child_idx_ = 0;
+    skipped_count_ = 0;
   }
 
   // Skip if ALL the nodes have been skipped
-  return all_skipped_ ? NodeStatus::SKIPPED : NodeStatus::FAILURE;
+  return (all_children_skipped) ? NodeStatus::SKIPPED : NodeStatus::FAILURE;
 }
 
 void FallbackNode::halt()
 {
   current_child_idx_ = 0;
+  skipped_count_ = 0;
   ControlNode::halt();
 }
 
--- src/controls/sequence_node.cpp
+++ src/controls/sequence_node.cpp
@@ -16,10 +16,7 @@
 namespace BT
 {
 SequenceNode::SequenceNode(const std::string& name, bool make_async)
-  : ControlNode::ControlNode(name, {})
-  , current_child_idx_(0)
-  , all_skipped_(true)
-  , asynch_(make_async)
+  : ControlNode::ControlNode(name, {}), current_child_idx_(0), asynch_(make_async)
 {
   if(asynch_)
     setRegistrationID("AsyncSequence");
@@ -30,6 +27,7 @@ SequenceNode::SequenceNode(const std::string& name, bool make_async)
 void SequenceNode::halt()
 {
   current_child_idx_ = 0;
+  skipped_count_ = 0;
   ControlNode::halt();
 }
 
@@ -37,9 +35,9 @@ NodeStatus SequenceNode::tick()
 {
   const size_t children_count = children_nodes_.size();
 
-  if(status() == NodeStatus::IDLE)
+  if(!isStatusActive(status()))
   {
-    all_skipped_ = true;
+    skipped_count_ = 0;
   }
 
   setStatus(NodeStatus::RUNNING);
@@ -50,9 +48,6 @@ NodeStatus SequenceNode::tick()
 
     auto prev_status = current_child_node->status();
     const NodeStatus child_status = current_child_node->executeTick();
-
-    // switch to RUNNING state as soon as you find an active child
-    all_skipped_ &= (child_status == NodeStatus::SKIPPED);
 
     switch(child_status)
     {
@@ -68,7 +63,7 @@ NodeStatus SequenceNode::tick()
       case NodeStatus::SUCCESS: {
         current_child_idx_++;
         // Return the execution flow if the child is async,
-        // to make this interruptable.
+        // to make this interruptible.
         if(asynch_ && requiresWakeUp() && prev_status == NodeStatus::IDLE &&
            current_child_idx_ < children_count)
         {
@@ -81,6 +76,7 @@ NodeStatus SequenceNode::tick()
       case NodeStatus::SKIPPED: {
         // It was requested to skip this node
         current_child_idx_++;
+        skipped_count_++;
       }
       break;
 
@@ -91,13 +87,15 @@ NodeStatus SequenceNode::tick()
   }    // end while loop
 
   // The entire while loop completed. This means that all the children returned SUCCESS.
+  const bool all_children_skipped = (skipped_count_ == children_count);
   if(current_child_idx_ == children_count)
   {
     resetChildren();
     current_child_idx_ = 0;
+    skipped_count_ = 0;
   }
   // Skip if ALL the nodes have been skipped
-  return all_skipped_ ? NodeStatus::SKIPPED : NodeStatus::SUCCESS;
+  return (all_children_skipped) ? NodeStatus::SKIPPED : NodeStatus::SUCCESS;
 }
 
 }  // namespace BT
